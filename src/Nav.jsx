6460d123d2603aec119ddb6e5a444515047ea29f
kongcmk <whoiskong@gmail.com>
2023-09-13 16:33:30 +0700

    fixed navbar

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,12 +1,14 @@
 const Navbar = () => {
     return (
-        <nav className="flex justify-end w-full px-8 py-2 mr-4 space-x-4 bg-white">
+        <nav className="flex items-center justify-end w-full px-8 py-2 mr-4 space-x-4 bg-white">
             {[
                 ['Home', '/'],
                 ['Owner', '/owner'],
             ].map(([title, url]) => (
                 <a href={url} className="px-3 py-2 font-medium rounded-lg text-slate-700 hover:bg-slate-100 hover:text-slate-900">{title}</a>
             ))}
+            <li><a href="/" className="px-3 py-2 font-medium rounded-lg text-slate-700 hover:bg-slate-100 hover:text-slate-900">Home</a></li>
+            <li><a href="owner" className="px-3 py-2 font-medium rounded-lg text-slate-700 hover:bg-slate-100 hover:text-slate-900">Owner</a></li>
         </nav>
     )
 }
