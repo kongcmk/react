8d53f2856abf8f532c0036e7175f8577d57b958a
kongcmk <whoiskong@gmail.com>
2023-09-13 17:35:53 +0700

    update files

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -7,8 +7,7 @@ const Navbar = () => {
             ].map(([title, url]) => (
                 <a href={url} className="px-3 py-2 font-medium rounded-lg text-slate-700 hover:bg-slate-100 hover:text-slate-900">{title}</a>
             ))}
-            <li><a href="/" className="px-3 py-2 font-medium rounded-lg text-slate-700 hover:bg-slate-100 hover:text-slate-900">Home</a></li>
-            <li><a href="/owner" className="px-3 py-2 font-medium rounded-lg text-slate-700 hover:bg-slate-100 hover:text-slate-900">Owner</a></li>
+        
         </nav>
     )
 }
